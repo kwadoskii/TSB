fd34cee799d8207751d7c315bd554db4932df68e
Austin Ofor <kwadoskii@live.com>
2021-09-01 16:40:01 +0100

    fix buggy comment component

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -5,7 +5,6 @@ import { HeartIcon as SolidHeartIcon } from "@heroicons/react/solid";
 import styled, { css } from "styled-components";
 import Link from "next/link";
 
-import Card from "./Card";
 import useVisible from "../hooks/useVisible";
 
 export default function Comment() {
@@ -25,15 +24,7 @@ export default function Comment() {
   ];
 
   return (
-    <Card
-      style={{
-        boxShadow: "none",
-        margin: "2.1em 0",
-        overflow: "unset",
-        // padding: "2em 5em",
-        padding: "0",
-      }}
-    >
+    <div className="bg-white my-4 p-0">
       <Details isOpen={isOpen} open>
         <Summary
           onClick={() => {
@@ -42,80 +33,84 @@ export default function Comment() {
         >
           <span>{!isOpen && "Austin Ofor + 1 replies"}</span>
         </Summary>
-        <div className="">
-          <div className="absolute h-8 w-8 top-1 -left-6">
-            <Link passHref href="/test">
-              <a>
+        <div className="flex gap-1">
+          <Link passHref href="/test">
+            <a>
+              <div className="relative w-8 h-8 top-0 left-0">
                 <Image
+                  alt="commenter"
                   src="https://res.cloudinary.com/practicaldev/image/fetch/s--HMZIR_Gv--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/555812/2bf2e16e-98a9-450b-af3a-1fbd51fce623.png"
                   layout="fill"
                   className="rounded-full"
                 />
-              </a>
-            </Link>
-          </div>
-          <Card style={{ padding: "0em", marginBottom: 0, marginLeft: "1em" }}>
-            <div className="flex p-2 flex-col">
-              <div className="flex justify-between mb-2">
-                <div className="flex space-x-1 items-center">
-                  <p className="font-semibold cursor-pointer hover:bg-gray-100 p-2 rounded-lg">
-                    Austin Ofor
-                  </p>
-                  <p className="text-gray-400">•</p>
-                  <span className="text-gray-500 text-sm">Jul 21</span>
-                </div>
-
-                <div
-                  onClick={handleShowMenu}
-                  ref={ref}
-                  className="cursor-pointer text-gray-400 hover:bg-gray-100 p-1 rounded-md self-center hover:text-gray-700 relative"
-                >
-                  <DotsHorizontalIcon className="h-5" />
-                </div>
               </div>
-              {isVisible && (
-                <div className="absolute right-0 top-10 z-[400]">
-                  <div className="bg-white border-black border-2 rounded-md overflow-hidden w-56 my-shadow-drop">
-                    <ul>
-                      {commentMenuData.map((menu, i) => (
-                        <Link passHref href={menu.url} key={i}>
-                          <a>
-                            <li className="p-2 cursor-pointer hover:bg-gray-100 hover:text-purple-500 rounded">
-                              {menu.name}
-                            </li>
-                          </a>
-                        </Link>
-                      ))}
-                    </ul>
+            </a>
+          </Link>
+
+          <div className="flex flex-col gap-1">
+            <div className="rounded-md p-0 border border-gray-300 mb-0 ml-4">
+              <div className="flex p-2 flex-col">
+                <div className="flex justify-between mb-2">
+                  <div className="flex space-x-1 items-center">
+                    <p className="font-semibold cursor-pointer hover:bg-gray-100 p-2 rounded-lg">
+                      Austin Ofor
+                    </p>
+                    <p className="text-gray-400">•</p>
+                    <span className="text-gray-500 text-sm">Jul 21</span>
+                  </div>
+
+                  <div
+                    onClick={handleShowMenu}
+                    ref={ref}
+                    className="cursor-pointer text-gray-400 hover:bg-gray-100 p-1 rounded-md self-center hover:text-gray-700 relative"
+                  >
+                    <DotsHorizontalIcon className="h-5" />
                   </div>
                 </div>
-              )}
+                {isVisible && (
+                  <div className="absolute right-0 top-10 z-[400]">
+                    <div className="bg-white border-black border-2 rounded-md overflow-hidden w-56 my-shadow-drop">
+                      <ul>
+                        {commentMenuData.map((menu, i) => (
+                          <Link passHref href={menu.url} key={i}>
+                            <a>
+                              <li className="p-2 cursor-pointer hover:bg-gray-100 hover:text-purple-500 rounded">
+                                {menu.name}
+                              </li>
+                            </a>
+                          </Link>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                )}
 
-              <div className="p-2 pt-1">
-                <p className="text-lg">
-                  Thanks, the SS redirect helped me. Lorem ipsum dolor, sit amet
-                  consectetur adipisicing elit. Obcaecati natus et ipsum adipisci, fugit
-                  quis doloremque, esse perferendis rem, distinctio nisi minima! Dicta,
-                  maiores. Dolores illo commodi animi iure aperiam.
-                </p>
+                <div className="p-2 pt-1">
+                  <p className="text-lg">
+                    Thanks, the SS redirect helped me. Lorem ipsum dolor, sit amet
+                    consectetur adipisicing elit. Obcaecati natus et ipsum adipisci, fugit
+                    quis doloremque, esse perferendis rem, distinctio nisi minima! Dicta,
+                    maiores. Dolores illo commodi animi iure aperiam.
+                  </p>
+                </div>
               </div>
             </div>
-          </Card>
 
-          <div className="px-4 mt-1 flex space-x-1 mb-5">
-            <div className="p-1 flex space-x-1 text-gray-600 text-sm items-center cursor-pointer px-3 hover:bg-gray-50 hover:text-gray-700 rounded-md bg-red-100">
-              {/* <HeartIcon className="h-5 text-gray-400" /> */}
-              <SolidHeartIcon className="h-5 text-red-600" />
-              <p>1 like</p>
-            </div>
-            <div className="p-1 flex space-x-1 text-gray-600 text-sm items-center cursor-pointer px-3 hover:bg-gray-50 hover:text-gray-700 rounded-md">
-              <ChatIcon className="h-5" />
-              <p>Reply</p>
+            <div className="px-4 mt-1 flex space-x-1 mb-5">
+              <div className="p-1 flex space-x-1 text-gray-600 text-sm items-center cursor-pointer px-3 hover:bg-gray-50 hover:text-gray-700 rounded-md bg-red-100">
+                {/* <HeartIcon className="h-5 text-gray-400" /> */}
+                <SolidHeartIcon className="h-5 text-red-600" />
+                <p>1 like</p>
+              </div>
+              <div className="p-1 flex space-x-1 text-gray-600 text-sm items-center cursor-pointer px-3 hover:bg-gray-50 hover:text-gray-700 rounded-md">
+                <ChatIcon className="h-5" />
+                <p>Reply</p>
+              </div>
             </div>
           </div>
         </div>
       </Details>
-    </Card>
+    </div>
   );
 }
 
@@ -129,8 +124,8 @@ const Details = styled.details`
        position: absolute;
        top: 40px;
        background: none;
-       left: -24px;
-       padding: 0 0.8em;
+       left: 0px;
+       padding: 0 0.9em;
     `}
 `;
 
