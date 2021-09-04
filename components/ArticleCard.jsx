72f828783ec3ed251996836de8dcf4818a3f7405
Austin Ofor <kwadoskii@live.com>
2021-09-04 17:19:27 +0100

    modified article card to support post owner menu and navbar menu alignment

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -1,33 +1,48 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { HeartIcon, ChatAltIcon } from "@heroicons/react/outline";
+import { HeartIcon, ChatAltIcon, DotsHorizontalIcon } from "@heroicons/react/outline";
+import useVisible from "../hooks/useVisible";
 
-export default function ArticleCard({ hasImage = false }) {
+export default function ArticleCard({ hasImage = false, userPost = false }) {
   const commentHandler = () => console.log("handler clicked");
   const likeHandler = () => console.log("handler clicked");
   const saveHandler = () => console.log("handler clicked");
 
+  const { isVisible, ref, setIsVisible } = useVisible();
+
   const tags = ["next", "tailwind", "javascript", "react"];
+  const menuData = [
+    { name: "Edit", url: "/edit" },
+    { name: "Delete", url: "/delete" },
+  ];
+
+  const handleShowMenu = () => {
+    setIsVisible(!isVisible);
+  };
 
   return (
-    <div className="w-full rounded-md border border-gray-300 overflow-hidden shadow-md mt-2">
+    <div className="w-full rounded-md border border-gray-300 overflow-hidden shadow-md mt-2 active:border-blue-700 relative">
       {hasImage && (
         <Link href="/kwadoskii/post-url" passHref>
           <a>
-            <ImageBanner
-              style={{
-                background: `url("https://res.cloudinary.com/practicaldev/image/fetch/s--rIMJB0Lh--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/881jdm7sdnril6hn3f3l.PNG") no-repeat center`,
-                backgroundSize: "cover",
-                width: "100%",
-                height: "230px",
-              }}
-            ></ImageBanner>
+            <div className="w-full h-[230px] relative">
+              <Image
+                alt="banner"
+                layout="fill"
+                objectFit="cover"
+                src="https://res.cloudinary.com/practicaldev/image/fetch/s--rIMJB0Lh--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/881jdm7sdnril6hn3f3l.PNG"
+              />
+            </div>
           </a>
         </Link>
       )}
 
-      <div className="bg-white rounded-md p-4 flex w-full gap-2 items-start focus:border-blue-700">
+      <div
+        className={`bg-white p-4 flex w-full gap-2 items-start ${
+          hasImage ? "rounded-b-md" : "rounded-md"
+        }`}
+      >
         <Link href="/kwadoskii" passHref>
           <a>
             <div className="w-9 h-9 relative border rounded-full">
@@ -48,11 +63,9 @@ export default function ArticleCard({ hasImage = false }) {
                 <p className="text-gray-800 text-sm font-medium py-2 pb-1 px-1 hover:bg-gray-50 inline-block rounded-md transition duration-100 ease-out -mt-3">
                   Test Arthur
                 </p>
-                <span className="text-xs text-gray-500 block px-1">
-                  May 6 (3 hours ago)
-                </span>
               </a>
             </Link>
+            <span className="text-xs text-gray-500 block px-1">May 6 (3 hours ago)</span>
           </div>
 
           <Link href="/kwadoskii/post-url" passHref>
@@ -102,6 +115,34 @@ export default function ArticleCard({ hasImage = false }) {
           </div>
         </div>
       </div>
+
+      {userPost && (
+        <div
+          onClick={handleShowMenu}
+          ref={ref}
+          className="cursor-pointer text-gray-400 hover:bg-gray-100 p-1 rounded-md self-center hover:text-gray-700 absolute top-1 right-3"
+        >
+          <DotsHorizontalIcon className="h-5" />
+        </div>
+      )}
+
+      {userPost && isVisible && (
+        <div className="absolute right-3 top-9 z-[400]">
+          <div className="bg-white border-black border-2 rounded-md overflow-hidden w-56 my-shadow-drop">
+            <ul>
+              {menuData.map((menu, i) => (
+                <Link passHref href={menu.url} key={i}>
+                  <a>
+                    <li className="p-2 cursor-pointer hover:bg-gray-100 hover:text-purple-500 rounded">
+                      {menu.name}
+                    </li>
+                  </a>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,54 +23,56 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="border-b border-gray-300 z-10 bg-white h-[56px] m-0 w-full flex justify-between px-1 gap-2 py-1 lg:py-4 lg:px-16 mx-auto">
-      <div className="flex justify-start items-center flex-1 gap-2">
-        <Link href="/">
-          <a>
-            <p className="cursor-pointer select-none bg-black p-[5px] rounded font-semibold text-white text-lg">
-              TSB
-            </p>
-          </a>
-        </Link>
+    <nav className="border-b border-gray-300 z-10">
+      <div className="bg-white h-[56px] m-0 w-full flex justify-between px-1 gap-2 py-1 lg:py-4 lg:px-6 mx-auto max-w-7xl relative">
+        <div className="flex justify-start items-center flex-1 gap-2">
+          <Link href="/">
+            <a>
+              <p className="cursor-pointer select-none bg-black p-[5px] rounded font-semibold text-white text-lg">
+                TSB
+              </p>
+            </a>
+          </Link>
 
-        <div className="flex-grow-0 w-full lg:w-1/2">
-          <form>
-            <Input
-              onChange={(e) => setSearchValue(e.target.value)}
-              placeholder="Search..."
-              value={searchValue}
-            />
-            <button type="submit" onClick={handleSearch} className="hidden"></button>
-          </form>
-        </div>
-      </div>
-
-      <div className="flex items-center">
-        <Link href="/write" passHref>
-          <a>
-            <button className="border-none p-3 bg-blue-700 text-white font-medium text-sm mr-3 cursor-pointer rounded-md hover:bg-blue-800 transition duration-100 ease-out">
-              Create post
-            </button>
-          </a>
-        </Link>
-        <div
-          className="relative w-10 h-10 cursor-pointer rounded-full none"
-          onClick={handleShowHideNavMenu}
-          ref={ref}
-        >
-          <Image
-            alt="user avatar"
-            className="rounded-full"
-            layout="fill"
-            src="https://res.cloudinary.com/practicaldev/image/fetch/s--qZUyVAzn--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/473848/c9176bd4-7e29-4848-84ca-534bb8533111.png"
-          />
-        </div>
-
-        {isVisible && (
-          <div className="absolute left-auto top-14 right-2 lg:right-16 bg-white my-shadow-drop text-black border-2 border-black rounded-md z-[400] sm:w-[250px] w-10/12">
-            <NavbarDropdown />
+          <div className="flex-grow-0 w-full lg:w-1/2">
+            <form>
+              <Input
+                onChange={(e) => setSearchValue(e.target.value)}
+                placeholder="Search..."
+                value={searchValue}
+              />
+              <button type="submit" onClick={handleSearch} className="hidden"></button>
+            </form>
           </div>
-        )}
+        </div>
+
+        <div className="flex items-center">
+          <Link href="/write" passHref>
+            <a>
+              <button className="border-none p-3 bg-blue-700 text-white font-medium text-sm mr-3 cursor-pointer rounded-md hover:bg-blue-800 transition duration-100 ease-out">
+                Create post
+              </button>
+            </a>
+          </Link>
+          <div
+            className="relative w-10 h-10 cursor-pointer rounded-full none"
+            onClick={handleShowHideNavMenu}
+            ref={ref}
+          >
+            <Image
+              alt="user avatar"
+              className="rounded-full"
+              layout="fill"
+              src="https://res.cloudinary.com/practicaldev/image/fetch/s--qZUyVAzn--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/473848/c9176bd4-7e29-4848-84ca-534bb8533111.png"
+            />
+          </div>
+
+          {isVisible && (
+            <div className="absolute left-auto top-14 right-2 lg:right-6 bg-white my-shadow-drop text-black border-2 border-black rounded-md z-[400] md:w-[250px] w-10/12">
+              <NavbarDropdown />
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
