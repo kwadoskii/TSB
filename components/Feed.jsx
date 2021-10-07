b30b213a69e7860621ac999fc4ee2e81556ef2d0
Austin Ofor <kwadoskii@live.com>
2021-10-07 13:48:39 +0100

    Mobile UI dropdown menu retain fix

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -8,18 +8,19 @@ export default function Feed({ tag = "", data = [..."hell"] }) {
   const path = router.asPath;
   const formattedTag = tag === "" ? "/" : "/t/" + tag + "/";
 
-  const [navs, setNavs] = useState([
-    { name: "Feed", postfixUrl: "" },
-    { name: "Week", postfixUrl: "top/week" },
-    { name: "Month", postfixUrl: "top/month" },
-    { name: "Year", postfixUrl: "top/year" },
-    { name: "Infinity", postfixUrl: "top/infinity" },
-    { name: "Latest", postfixUrl: "top/latest" },
+  const [navs, _] = useState([
+    { name: "Feed", sortUrl: "" },
+    { name: "Week", sortUrl: "top/week" },
+    { name: "Month", sortUrl: "top/month" },
+    { name: "Year", sortUrl: "top/year" },
+    { name: "Infinity", sortUrl: "top/infinity" },
+    { name: "Latest", sortUrl: "top/latest" },
   ]);
-  const [selected, setSelected] = useState("home");
+  const [selected, setSelected] = useState("");
 
   useEffect(() => {
-    if (path.split("/").length <= 3) return setSelected("feed");
+    if ((tag && path.split("/").length === 3) || path.split("/").length === 2)
+      return setSelected("feed");
 
     setSelected(path.split("/").pop());
   }, []);
@@ -28,8 +29,8 @@ export default function Feed({ tag = "", data = [..."hell"] }) {
     <div>
       <div className="flex justify-between items-center">
         <h3 className="text-xl text-gray-800 font-bold">Posts</h3>
-        <div className="hidden sm:flex gap-1">
-          {navs.map(({ name, postfixUrl }) => (
+        <div className="hidden sm:grid grid-flow-col gap-0">
+          {navs.map(({ name, sortUrl }) => (
             <div
               key={name}
               className={`my-feed-link ${selected === name.toLocaleLowerCase() && "font-bold text-blue-700"}`}
@@ -64,15 +65,15 @@ export default function Feed({ tag = "", data = [..."hell"] }) {
         </div>
 
         <select
-          onChange={(e) => router.push(tag + e.target.value)}
+          onChange={(e) => router.push(formattedTag + e.target.value)}
           className="inline-block sm:hidden p-2 outline-none border border-gray-300 rounded-md active:border-blue-700"
+          value={selected !== "" ? `top/${selected}` : ""}
         >
-          <option value="/">Feed</option>
-          <option value="/top/week">Week</option>
-          <option value="/top/month">Month</option>
-          <option value="/top/year">Year</option>
-          <option value="/top/infinity">Infinity</option>
-          <option value="/top/latest">Latest</option>
+          {navs.map(({ name, sortUrl }, i) => (
+            <option value={sortUrl} key={i}>
+              {name}
+            </option>
+          ))}
         </select>
       </div>
 
