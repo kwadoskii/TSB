ac33b08d974305d6f72dae2071d051f000d0b1e3
Austin Ofor <kwadoskii@live.com>
2022-01-31 08:59:44 +0100

    tag dynamism follow and unfollow

--- components/TagCard.jsx
+++ components/TagCard.jsx
@@ -1,10 +1,46 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
-export default function TagCard({ barColor, name, subtitle, large = false }) {
+export default function TagCard({
+  barColor,
+  name,
+  subtitle,
+  large = false,
+  image,
+  followed,
+  onFollow,
+  onUnfollow,
+  token,
+}) {
+  const [_followed, setFollowed] = useState(followed);
+  const handleFollow = async () => {
+    setFollowed(true);
+
+    const { data, status } = await onFollow();
+    if (status === 200 && data.status === "success") return;
+
+    setFollowed(false);
+    return toast.error(data.message);
+  };
+
+  const handleUnfollow = async () => {
+    setFollowed(false);
+
+    const { data, status } = await onUnfollow();
+    if (status === 200 && data.status === "success") return;
+
+    setFollowed(true);
+    return toast.error(data.message);
+  };
+
   return (
     <div>
-      <div className={`rounded-t-md ${large ? "h-4" : "h-2"}`} style={{ backgroundColor: barColor }}></div>
+      <div
+        className={`rounded-t-md ${large ? "h-4" : "h-2"}`}
+        style={{ backgroundColor: barColor }}
+      ></div>
       <div className="rounded-b-md border-b border-gray-200 bg-white shadow-sm relative overflow-hidden">
         {!large ? (
           <div className="px-4 py-4 flex flex-col gap-1">
@@ -17,21 +53,28 @@ export default function TagCard({ barColor, name, subtitle, large = false }) {
               </a>
             </Link>
 
-            <div className="mb-2">{subtitle && <p className="text-gray-500 text-sm line-clamp-3">{subtitle}</p>}</div>
+            <div className="mb-2">
+              {subtitle && <p className="text-gray-500 text-sm line-clamp-3">{subtitle}</p>}
+            </div>
           </div>
         ) : (
           <div className="relative p-6 flex gap-4">
             <div className="relative h-14 w-14 rotate-6 hidden lg:block">
-              <Image
-                src="https://res.cloudinary.com/practicaldev/image/fetch/s--e4kQ9nis--/c_limit,f_auto,fl_progressive,q_80,w_64/https://dev-to-uploads.s3.amazonaws.com/uploads/badge/badge_image/16/js-badge.png"
-                layout="fill"
-                objectFit="cover"
-              />
+              {image && <Image src={image} layout="fill" objectFit="cover" />}
             </div>
             <div className="flex-grow">
               <div className="flex gap-2 justify-between items-center">
                 <h3 className="text-3xl font-bold">{name}</h3>
-                <button className="my-button px-5 m-0 py-2 text-base">Follow</button>
+                {token && (
+                  <button
+                    className={`border-none p-3 bg-transparent text-blue-700 font-semibold text-sm cursor-pointer rounded-md hover:bg-blue-200/30 transition duration-100 ease-out self-end items-start ring-1 ring-blue-600 ${
+                      !_followed && "!bg-blue-600 !text-white hover:!bg-blue-600/80"
+                    }`}
+                    onClick={_followed ? () => handleUnfollow() : () => handleFollow()}
+                  >
+                    {_followed ? "Following" : "Follow"}
+                  </button>
+                )}
               </div>
 
               <p className="pt-5 md:w-9/12 text-gray-800">{subtitle}</p>
--- pages/t/[tag]/index.jsx
+++ pages/t/[tag]/index.jsx
@@ -1,29 +1,56 @@
-import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import authService from "../../../apis/authService";
+import { getTagByName } from "../../../apis/tag";
+import { reactions, getUserFollowingTags, followTag, unfollowTag } from "../../../apis/user";
 import Advert from "../../../components/Advert";
 import Feed from "../../../components/Feed";
 import Navbar from "../../../components/Navbar";
 import TagCard from "../../../components/TagCard";
 import Title from "../../../components/Title";
 
-export default function TagPage() {
-  const { query } = useRouter();
-  const tag = query.tag;
+export default function TagPage({ followingTags, top = false }) {
+  const [tag, setTag] = useState();
+  const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState(true);
+  const { query, replace } = useRouter();
+  const tagName = query.tag;
 
-  return (
+  useEffect(async () => {
+    setLoading(true);
+
+    if (!top) {
+      const {
+        data: { data: _tag },
+        status,
+      } = await getTagByName(tagName);
+
+      if (status !== 200) return replace("/404");
+    }
+
+    setToken(authService.getJwt());
+    setTag(_tag);
+    setLoading(false);
+  }, []);
+
+  return loading ? null : (
     <>
-      <Title title={tag} />
+      <Title title={tag?.name} />
       <Navbar />
 
       <div className="bg-gray-100 my-min-height">
         <div className="max-w-7xl relative px-1 py-1 lg:py-4 lg:px-6 mx-auto">
           <div>
             <TagCard
-              barColor="gold"
-              name={query.tag}
-              subtitle="lorem ispsum dolum test one to three"
+              barColor={tag?.backgroundColor}
+              name={tag?.name}
+              subtitle={tag?.description}
+              image={tag?.image}
               large
+              followed={followingTags.some((ft) => ft._id === tag._id)}
+              onFollow={() => followTag(tag._id, token)}
+              onUnfollow={() => unfollowTag(tag._id, token)}
+              token={token}
             />
           </div>
 
@@ -40,4 +67,25 @@ export default function TagPage() {
       </div>
     </>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  // const tagName = req.url.split("/").reverse()[0];
+  const { token } = req.cookies;
+  let followingTags;
+
+  if (!token) {
+    followingTags = [];
+  } else {
+    const {
+      data: { data: _tag },
+    } = await getUserFollowingTags(req.cookies.token);
+    followingTags = _tag.tags;
+  }
+
+  return {
+    props: {
+      followingTags,
+    },
+  };
 }
