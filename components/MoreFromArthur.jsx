8ca811ab1d8435c2cb334ffbd617a2d53fdc9275
Austin Ofor <kwadoskii@live.com>
2022-02-11 19:26:18 +0100

    minor patch on comment and morefrom arthur

--- components/MoreFromArthur.jsx
+++ components/MoreFromArthur.jsx
@@ -1,45 +1,12 @@
 import Link from "next/link";
 
 export default function MoreFromArthur({ profile, previousPosts, numberOfPosts = 5 }) {
-  // previousPosts = [
-  //   {
-  //     _id: "one",
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     url: "/jwad/i-love-javascript",
-  //     tags: [{ name: "politics" }, { name: "football" }, { name: "music" }, { name: "crime" }],
-  //   },
-  //   {
-  //     _id: "two",
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     url: "/jwad/i-love-javascript",
-  //     tags: [{ name: "politics" }, { name: "football" }, { name: "music" }, { name: "crime" }],
-  //   },
-  //   {
-  //     _id: "three",
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     url: "/jwad/i-love-javascript",
-  //     tags: [{ name: "politics" }, { name: "football" }, { name: "music" }, { name: "crime" }],
-  //   },
-  //   {
-  //     _id: "four",
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     url: "/jwad/i-love-javascript",
-  //     tags: [{ name: "politics" }, { name: "football" }, { name: "music" }, { name: "crime" }],
-  //   },
-  //   {
-  //     _id: "five",
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     url: "/jwad/i-love-javascript",
-  //     tags: [{ name: "politics" }, { name: "football" }, { name: "music" }, { name: "crime" }],
-  //   },
-  // ];
-
   if (Array.isArray(previousPosts) && previousPosts?.length === 0) return null;
 
   return (
-    <div className="flex flex-col border-gray-300 rounded-md bg-gray-50 mt-3 border last:border-b-0 overflow-hidden shadow-md">
-      <div className="border-b border-gray-300 p-4">
-        <h2 className="font-bold text-xl">
+    <div className="flex flex-col mt-3 bg-gray-50 border last:border-b-0 border-gray-300 rounded-md shadow-md overflow-hidden">
+      <div className="p-4 border-b border-gray-300">
+        <h2 className="text-xl font-bold">
           More from{" "}
           <Link passHref href={`/${profile.username}`}>
             <a className="text-blue-700">{profile.firstname + " " + profile.lastname}</a>
@@ -49,14 +16,14 @@ export default function MoreFromArthur({ profile, previousPosts, numberOfPosts =
 
       {previousPosts?.slice(0, numberOfPosts).map((post) => (
         <div
-          className="border-b border-gray-300 p-4 cursor-pointer text-gray-800 hover:bg-white hover:text-blue-600"
+          className="p-4 hover:text-blue-600 text-gray-800 hover:bg-white border-b border-gray-300 cursor-pointer"
           key={post._id.toString()}
         >
           <Link passHref href={`/${profile.username}/${post.slug}`}>
             <a>
               <p>{post.title}</p>
 
-              <div className="flex flex-wrap my-2 gap-2 text-gray-500 text-sm">
+              <div className="flex flex-wrap gap-2 my-2 text-gray-500 text-sm">
                 {post.tags?.map((tag, i) => (
                   <p key={i}>
                     <span className="text-gray-300">#</span>
--- components/ProfileComment.jsx
+++ components/ProfileComment.jsx
@@ -7,15 +7,15 @@ export default function ProfileComment({ comment, profileUrl }) {
   formatedCreatedAt =
     dayjs().format("YYYY") === dayjs(comment.createdAt).format("YYYY")
       ? dayjs(comment.createdAt).format("MMM DD")
-      : dayjs(comment.createdAt).format("YYYY MMM DD");
+      : dayjs(comment.createdAt).format("MMM DD, YYYY");
 
   return (
     <Link href={`/${profileUrl}/comment/${comment?._id}`} passHref>
-      <a className="hover:bg-gray-50 py-4 px-4 border-b border-gray-300 flex flex-col gap-2 last:border-b-0">
+      <a className="flex flex-col gap-2 px-4 py-4 hover:bg-gray-50 border-b last:border-b-0 border-gray-300">
         <h4 className="font-bold">{comment.postId?.title}</h4>
         <div className="flex gap-2 items-center text-sm">
           <p className="text-gray-700">{comment.comment}</p>
-          <span className="text-gray-500">{formatedCreatedAt}</span>
+          <span className="text-gray-500 text-xs">{formatedCreatedAt}</span>
         </div>
       </a>
     </Link>
