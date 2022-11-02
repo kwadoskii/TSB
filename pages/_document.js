943add2ee37a7f4a7cad521fcec09054cb4eda99
Austin Ofor <30274314+kwadoskii@users.noreply.github.com>
2022-11-02 10:38:56 +0100

    minor tweak on SEO

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import config from "../constants/config.json";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,14 +11,13 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="author" content="Ofor Chikwado Augustine"></meta>
+          <meta name="developed-by" content="Ofor Chikwado Augustine"></meta>
+          <meta name="keywords" content={config.appName.keywords.join(", ")}></meta>
+
           {/* use the below for each pages's tag */}
-          <meta name="keywords" content="HTML, CSS, JavaScript"></meta>
-          <meta name="author" content="TutorialBrain"></meta>
-          <meta
-            name="description"
-            content="Online Tutorials for all type of brains"
-          ></meta>
+          {/* ? move this to the individual post page */}
+          {/* <meta name="author" content="TutorialBrain"></meta> */}
+          {/* <meta name="description" content="Online Tutorials for all type of brains"></meta> */}
         </Head>
         <body>
           <Main />
