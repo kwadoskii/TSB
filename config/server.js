43f17bfa0b458b9833ea25191646786180deee23
Austin Ofor <30274314+kwadoskii@users.noreply.github.com>
2022-12-05 20:03:50 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://t.d:9000/api" : "https://tsb-backend.herokuapp.com/api";
+export const server = dev ? "http://t.d:9000/api" : process.env.NEXT_PUBLIC_BE_ADD;
